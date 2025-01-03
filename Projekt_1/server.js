ed3a5532071142a2e7439f97b98b7347e547e654
Lam Nguyen <125300659+NotAisu@users.noreply.github.com>
2025-01-04 00:00:22 +0100

    experimenting with express

--- Projekt_1/server.js
+++ Projekt_1/server.js
@@ -2,4 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    console.log("yay i hit an endpoint", req.method);
+    res.sendStatus(201);
+})
+
+app.get("/dashboard", (req, res) => {
+    console.log("yooo i hit /dashboard endpoint", req.method);
+    res.send("Welcome to the dashboard");
+})
+
 app.listen(PORT, () => console.log(`Server has started on ${PORT}`))
