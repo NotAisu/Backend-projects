5a965f223a4037f0b9a3a2d744cc31700dd55959
Lam Nguyen <125300659+NotAisu@users.noreply.github.com>
2025-02-08 15:05:40 +0100

    crud und rest client gelernt

--- Projekt_1/server.js
+++ Projekt_1/server.js
@@ -1,15 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
+let data = ["Lam"];
+
+// Middleware
+app.use(express.json());
+
+// Type 1Website endpoint (these endpoints are for sending back html code)
+
 app.get("/", (req, res) => {
-    console.log("yay i hit an endpoint", req.method);
-    res.sendStatus(201);
-})
+  console.log("User requested the home page");
+  res.send(`
+    <body 
+    style="background: pink;
+    color: blue;">
+        <h1>DATA:</h1>
+        <p> ${JSON.stringify(data)}</p>
+        <a href="/dashboard">Dashboard</a>
+    </body>
+    <script>console.log("This is my script")</script>
+    `);
+});
 
 app.get("/dashboard", (req, res) => {
-    console.log("yooo i hit /dashboard endpoint", req.method);
-    res.send("Welcome to the dashboard");
-})
+  res.send(`
+    <body>
+    
+    <h1>Dashboard</h1>
+    <a href="/">Home</a>
+    </body>
+    `);
+});
 
-app.listen(PORT, () => console.log(`Server has started on ${PORT}`))
+// Type 2 API endpoint (non visual)
+
+//CRUD(Method) - Create(post), Read(get), Update(put), Delete(delete)
+
+app.get("/api/data", (req, res) => {
+  console.log("This one was for data");
+  res.status(599).send(data);
+});
+// req.body ist inhalt des request
+app.post("/api/data", (req, res) => {
+  // somebody wants to create a user (bsp signup)
+  // user clicks on a button after filling out a form
+  const newEntry = req.body;
+  console.log(newEntry);
+  data.push(newEntry.name);
+  res.sendStatus(201);
+});
+
+app.delete("/api/data", (req, res) => {
+  data.pop();
+  console.log("we deleted the last element");
+  res.sendStatus(204);
+});
+
+app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
+
